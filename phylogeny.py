371318170b745c544ecc06b638b012b13a43c8cb
m006703 <sakai.yuta@mayo.edu>
2019-10-22 22:18:07 -0500

    added feature to calculate the q matrix and figure out the min q distance from the q matrix

--- phylogeny.py
+++ phylogeny.py
@@ -60,6 +60,46 @@ def main():
         genetic_distance_file.write("\t".join(val))
         genetic_distance_file.write("\n")
 
+    # Calculate the Q matrix and store the Q distances in a dictionary
+    Q_matrix_dict = {}
+    for i in range(0, len(identifiers)):
+        Q_matrix_dict[identifiers[i]] = []
+        # Calculate the sum of distances to the ith sequence
+        sum_distance_i_k = sum(genetic_distance_dict[identifiers[i]])
+        for j in range(0, len(identifiers)):
+            # No need to calculate Q for the same sequences
+            # Have the Q_distance be 0 as a placeholder
+            if i == j:
+                Q_matrix_dict[identifiers[i]].append(0)
+            else:
+                # Calculate the sum of distances to the jth sequence
+                sum_distance_j_k = sum(genetic_distance_dict[identifiers[j]])
+                # Calculate the Q distance
+                Q_distance = ((len(identifiers) - 2) * genetic_distance_dict[identifiers[i]][j] -
+                              sum_distance_i_k - sum_distance_j_k)
+                # Append the Q distance as a list, which will be the values for the ith sequence key
+                Q_matrix_dict[identifiers[i]].append(Q_distance)
+
+    # Find the minimum distance in the Q matrix
+    # Initialize the overall minimum Q distance as 0
+    min_q_distance = 0
+    for key in Q_matrix_dict.keys():
+        # Check the minimum Q distance for a given sequence
+        min_q_distance_for_sequence = min(Q_matrix_dict[key])
+        # If the minimum Q distance for the sequence is smaller than the overall min Q distance,
+        # replace the overall minimum Q distance
+        if min_q_distance_for_sequence < min_q_distance:
+            min_q_distance = min_q_distance_for_sequence
+            # Store the sequence (key) where this happens
+            min_q_distance_key = key
+            # Find and store the index of the min Q distance in the list of values for the key
+            min_q_distance_index = Q_matrix_dict[key].index(min_q_distance)
+            # See against which sequence the min Q value was calculated relative to the key
+            min_q_distance_partner = identifiers[min_q_distance_index]
+    print(min_q_distance)
+    print(min_q_distance_key)
+    print(min_q_distance_partner)
+
     fasta_file.close()
     genetic_distance_file.close()
 
